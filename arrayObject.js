22a653aff6ca523a9440c412fc9f923a30b0b3f5
Ramsey <seamusapple@163.com>
2022-05-03 00:39:35 +0800

    Map function for array

--- arrayObject.js
+++ arrayObject.js
@@ -175,3 +175,34 @@ console.log(
 //   { name: "王五", age: 28  },
 //   { name: "张三", age: 30 }
 // ]
+
+// map()方法将数组的所有成员依次传入参数函数， 然后把每一次的执行结果组成一个新数组返回。
+var numbers = [1, 2, 3];
+numbers.map(function (n) {
+  return n + 1;
+}); // [2, 3, 4]
+console.log(numbers); // [1, 2, 3]
+
+// map()方法接受一个函数作为参数。
+// 该函数调用时，map()方法向它传入三个参数：当前成员、当前位置和数组本身
+[1, 2, 3].map(function (elem, index, arr) {
+  return elem * index;
+}); // [0, 2, 6];
+
+// map()方法还可以接受第二个参数，用来绑定回调函数内部的this变量
+var arr = ["a", "b", "c"];
+console.log(
+  [1, 2].map(function (e) {
+    return this[e];
+  }, arr)
+); // ['b', 'c']
+// 上面代码通过map()方法的第二个参数，将回调函数内部的this对象，指向arr数组。
+
+// 如果数组有空位，map()方法的回调函数在这个位置不会执行，会跳过数组的空位。
+var f = function (n) {
+  return "a";
+};
+console.log([1, undefined, 2].map(f)); // ['a', 'a', 'a'];
+console.log([1, null, 2].map(f)); // ['a', 'a', 'a'];
+console.log([1, , 2].map(f)); // ["a", "a"];
+// 上面代码中，map()方法不会跳过undefined和null，但是会跳过空位
