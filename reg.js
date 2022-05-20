9d5f175c8b2927e363a2b99a5c1966420da4757e
Dong Pan <seamusapple@163.com>
2022-05-20 13:55:48 +0800

    finish regex some patterns

--- reg.js
+++ reg.js
@@ -236,3 +236,178 @@ console.log("aaa**a*".split(/a*/)); // [ '', '*', '*', '*' ]
 
 // 如果正则表达式带有括号，则括号匹配的部分也会作为数组成员返回
 console.log("aaa*a*".split(/(a*)/)); // [ '', 'aaa', '*', 'a', '*' ]
+
+// 字面量字符和元字符
+// metacharacters
+// 1.点字符(.)
+// 点字符（.）匹配除回车（\r）、换行(\n) 、行分隔符（\u2028）和段分隔符（\u2029）以外的所有字符。
+// 注意，对于码点大于0xFFFF字符，点字符不能正确匹配，会认为这是两个字符。
+/c.t/;
+// c.t匹配c和t之间包含任意一个字符的情况。 只要三个字符在同一行，就可以匹配，如car、c2t、c-t.但不能匹配四个字符，如coot.
+
+// 2.任意字符
+// 位置字符用来提示字符所处的位置，主要有两个字符。
+// ^ 表示字符串的开始位置
+// $ 表示字符串的结束位置
+// test必须出现在开始位置
+console.log(/^test/.test("test123")); // true
+console.log(/^test/.test("new test")); // false
+// test必须出现在结束位置
+console.log(/test$/.test("new test")); // true
+console.log(/test$/.test("test123")); // false
+// 从开始位置到结束位置只有test
+console.log(/^test$/.test("test")); // true
+console.log(/^test$/.test("test123")); // false
+console.log(/^test$/.test("new test")); // false
+
+// 3. 选择符(|)
+// 竖线符号（|）在正则表达式中表示“或关系”（OR），即cat|dog表示匹配cat或dog
+console.log(/11|22/.test("911")); // true
+// 多个选择符可以联合使用
+console.log(/freg|barney|betty/.test("betty1")); // true
+// 选择符会包括它前后的多个字符，比如/ab|cd/指的是匹配ab或者cd，而不是指匹配b或者c。如果想修改这个行为，可以使用圆括号。
+console.log(/a( |\t)b/.test("a\tb")); // true
+// a和b之间有一个空格或者一个制表符
+
+// 转义符
+// 正则表达式中那些有特殊含义的元字符，如果要匹配它们本身，就需要在它们前面要加上反斜杠。比如要匹配+，就要写成\+
+console.log(/1+1/.test("1+1")); // false
+console.log(/1\+1/.test("1+1")); // true
+
+// 正则表达式中，需要反斜杠转义的，一共有12个字符：^、.、[、$、(、)、|、*、+、?、{和\
+// 需要特别注意的是，如果使用RegExp方法生成正则对象，转义需要使用两个斜杠，因为字符串内部会先转义一次。
+console.log(new RegExp("1+1").test("1+1")); // false
+console.log(new RegExp("1+\\+1").test("1+1")); // false
+
+// 特殊字符
+// 正则表达式对一些不能打印的特殊字符，提供了表达方法。
+/*
+\cX 表示Ctrl-[X]，其中的X是A-Z之中任一个英文字母，用来匹配控制字符。
+[\b] 匹配退格键(U+0008)，不要与\b混淆。
+\n 匹配换行键。
+\r 匹配回车键。
+\t 匹配制表符 tab（U+0009）。
+\v 匹配垂直制表符（U+000B）。
+\f 匹配换页符（U+000C）。
+\0 匹配null字符（U+0000）。
+\xhh 匹配一个以两位十六进制数（\x00-\xFF）表示的字符。
+\uhhhh 匹配一个以四位十六进制数（\u0000-\uFFFF）表示的 Unicode 字符。
+*/
+
+// 字符类
+// 字符类（class）表示有一系列字符可供选择，只要匹配其中一个就可以了。
+// 所有可供选择的字符都放在方括号内，比如[xyz] 表示x、y、z之中任选一个匹配。
+console.log(/[abc]/.test("hello world")); // false
+console.log(/[abc]/.test("apple")); // true
+// 1. 脱字符（^）
+// 如果方括号内的第一个字符是[^]，则表示除了字符类之中的字符，其他字符都可以匹配。比如，[^xyz]表示除了x、y、z之外都可以匹配。
+console.log(/[^abc]/.test("bbc news")); // true
+console.log(/[^abc]/.test("bbc")); // false
+
+// 如果方括号内没有其他字符，即只有[^]，就表示匹配一切字符，其中包括换行符。相比之下，点号作为元字符（.）是不包括换行符的
+var s = "Please yes\nmake my day!";
+console.log(s.match(/yes.*day/)); // null
+console.log(s.match(/yes[^]*day/));
+/*
+[
+  'yes\nmake my day',
+  index: 7,
+  input: 'Please yes\nmake my day!',
+  groups: undefined
+]
+ */
+// 注意，脱字符只有在字符类的第一个位置才有特殊含义，否则就是字面含义
+
+// 2. 连字符 (-)
+// 某些情况下，对于连续序列的字符，连字符（-）用来提供简写形式，表示字符的连续范围。
+// 比如，[abc]可以写成[a-c]，[0123456789]可以写成[0-9]，同理[A-Z]表示26个大写字母。
+console.log(/a-z/.test("b")); // false
+console.log(/[a-z]/.test("b")); // true
+// 当连字号（dash）不出现在方括号之中，就不具备简写的作用，只代表字面的含义，所以不匹配字符b。
+// 只有当连字号用在方括号之中，才表示连续的字符序列。
+/*
+[0-9.,]
+[0-9a-fA-F]
+[a-zA-Z0-9-]
+[1-31]
+*/
+// 上面代码中最后一个字符类[1-31]，不代表1到31，只代表1到3
+// 连字符还可以用来指定 Unicode 字符的范围
+var str = "\u0130\u0131\u0132";
+console.log(/[\u0128-\uFFFF]/.test(str)); // true
+
+// 预定义模式
+// 预定义模式指的是某些常见模式的简写方式
+/*
+\d 匹配0-9之间的任一数字，相当于[0-9]。
+\D 匹配所有0-9以外的字符，相当于[^0-9]。
+\w 匹配任意的字母、数字和下划线，相当于[A-Za-z0-9_]。
+\W 除所有字母、数字和下划线以外的字符，相当于[^A-Za-z0-9_]。
+\s 匹配空格（包括换行符、制表符、空格符等），相等于[ \t\r\n\v\f]。
+\S 匹配非空格的字符，相当于[^ \t\r\n\v\f]。
+\b 匹配词的边界。
+\B 匹配非词边界，即在词的内部。
+*/
+// \s 的例子
+console.log(/\s\w*/.exec("hello world")); // [ ' world', index: 5, input: 'hello world', groups: undefined ]
+// \b 的例子
+console.log(/\bworld/.test("hello world")); // true
+console.log(/\bworld/.test("hello-world")); // true
+console.log(/\bworld/.test("helloworld")); // false
+// \B 的例子
+console.log(/\Bworld/.test("hello world")); // false
+console.log(/\Bworld/.test("helloworld")); // true
+// 通常，正则表达式遇到换行符（\n）就会停止匹配
+var html = "<b>Hello</b>\n<i>world!</i>";
+console.log(/.*/.exec(html)[0]); // <b>Hello</b>
+// 上面代码中，字符串html包含一个换行符，结果点字符（.）不匹配换行符，导致匹配结果可能不符合原意。
+// 这时使用\s字符类，就能包括换行符。
+console.log(/[\S\s]*/.exec(html)[0]);
+// <b>Hello</b>\n<i>world!</i>
+// 上面代码中，[\S\s]指代一切字符
+
+// 重复类
+// 模式的精确匹配次数，使用大括号（{}）表示。
+// {n}表示恰好重复n次，{n,}表示至少重复n次，{n,m}表示重复不少于n次，不多于m次。
+console.log(/lo{2}k/.test("look")); // true
+console.log(/lo{2,5}k/.test("looook")); // true
+
+// 量词符
+// 量词符用来设定某个模式出现的次数。
+/*
+? 问号表示某个模式出现0次或1次，等同于{0, 1}。
+* 星号表示某个模式出现0次或多次，等同于{0,}。
++ 加号表示某个模式出现1次或多次，等同于{1,}。
+*/
+// t 出现0次或1次
+console.log(/t?est/.test("test")); // true
+console.log(/t?est/.test("est")); // true
+
+// t 出现1次或多次
+console.log(/t+est/.test("test")); // true
+console.log(/t+est/.test("ttest")); // true
+console.log(/t+est/.test("est")); // false
+
+// t 出现0次或多次
+console.log(/t*est/.test("test")); // true
+console.log(/t*est/.test("ttest")); // true
+console.log(/t*est/.test("tttest")); // true
+console.log(/t*est/.test("est")); // true
+
+// 贪婪模式
+// 三个量词符，默认情况下都是最大可能匹配，即匹配到下一个字符不满足匹配规则为止。这被称为贪婪模式
+var s = "aaa";
+console.log(s.match(/a+/)); // [ 'aaa', index: 0, input: 'aaa', groups: undefined ]
+// 上面代码中，模式是/a+/，表示匹配1个a或多个a，那么到底会匹配几个a呢？因为默认是贪婪模式，会一直匹配到字符a不出现为止，所以匹配结果是3个a
+// 除了贪婪模式，还有非贪婪模式，即最小可能匹配。只要一发现匹配，就返回结果，不要往下检查。如果想将贪婪模式改为非贪婪模式，可以在量词符后面加一个问号
+console.log(s.match(/a+?/)); // [ 'a', index: 0, input: 'aaa', groups: undefined ]
+// 上面例子中，模式结尾添加了一个问号/a+?/，这时就改为非贪婪模式，一旦条件满足，就不再往下匹配，+?表示只要发现一个a，就不再往下匹配了。
+/*
++?：表示某个模式出现1次或多次，匹配时采用非贪婪模式。
+*?：表示某个模式出现0次或多次，匹配时采用非贪婪模式。
+??：表格某个模式出现0次或1次，匹配时采用非贪婪模式
+*/
+console.log("abb".match(/ab*/)); // [ 'abb', index: 0, input: 'aaa', groups: undefined ]
+console.log("abb".match(/ab*?/)); // [ 'a', index: 0, input: 'aaa', groups: undefined ]
+console.log("abb".match(/ab?/)); // [ 'ab', index: 0, input: 'abb', groups: undefined ]
+console.log("abb".match(/ab??/)); // [ 'a', index: 0, input: 'abb', groups: undefined ]
